9b0035a7ad23ad98937da752c3144f674550ec25
Joe Walker <joe at getahead dot org>
2009-01-07 15:36:07 +0000

    final move around to final (hopefully) file destinations

---
+++ readme.txt
@@ -0,0 +1,98 @@
+
+DOCDWR
+------
+
+This project contains the documentation for DWR.
+The documentation is stored in HTML files suitable for editing in any HTML
+editor. There is a publishing process that prepares the files (by adding
+certain header/footer/menu elements)
+
+
+License
+-------
+
+This project is not currently open sourced. All rights to the documentation
+provided is preserved by its authors. This means that you do not have the right
+to redistribute, build on, copy or publish any of the material in this project.
+The official publishing location is under http://directwebremoting.org/
+This restriction is in place to help the authors to potentially publish a book
+using this content.
+
+
+Editing Rules
+-------------
+In order to ensure that the publishing process works properly, there are some
+rules for editing the documentation:
+- All links must be relative
+- Please keep to the same formatting style as the html around you. In general
+  this means that most content will be formatted with a paragraph on a single
+  line with open and closing tags on the same line. Paragraphs are then
+  separated by a single blank line. This style means that the HTML is fairly
+  readable in any text editor that supports word wrapping.
+  In general elements other than tables are not indented. When indenting use
+  2 space characters and no tabs
+- Please keep the HTML very simple
+  For the most part p/h1/h2/h3/a/strong/pre/code are all the tags you should need
+- If you want to move a page from one location to another, please:
+  - Ensure that all inbound links point to the new location
+  - In the header of the moved document, add a line that points to the old location
+    This enables us to create an Apache config file that prevents needless 404s
+- All pages should have an HTML5 doctype: <!DOCTYPE html>
+- All pages should be stored with a .html file extension
+- All elements/attributes should be lower case and should be valid html5
+- In addition to normal html5 rules, the documents should be well-formed XML
+  (although they should not include an XML declaration <?xml version="1.0"...
+  and they do not need to be valid beyond the html5 rules)
+  This may help us process the documents using XSL or similar in the future
+- All directories must contain an index.html file that links to all of the
+  HTML files within that directory (unless the files are supposed to be hidden)
+- All images must be stored in the dwr/images directory
+- Page titles should be succinct particularly in index.html files, because they
+  are used in the breadcrumbs and menu structure
+
+
+Templating Process
+------------------
+
+The templating process reads the source files in docs/web, and then writes them
+to target/template with additional templating.
+A source page is of roughly the following format
+
+HEADER LINES
+ *</head> *
+ *<body.*
+BODY LINES
+ *</body> *
+ *</html>
+
+There are then 3 insertion points.
+- At the end of the header (used for inserting additional css/scripts/etc.)
+- At the top of the body (header/breadcrumbs)
+- At the bottom of the body (footer/menu/scripts/etc)
+
+There are, then 7 parts to the output document
+- Page Header
+    Everything in the source page up to (but not including) </head>
+    Stored in the Page.header member variable
+- Template Header Insert
+    Fetched from the getTemplateHeaderInsert() method
+- Page Neck
+    Everything in the source page from </head> to <body> (inclusive)
+    Stored in the Page.neck member variable
+- Template PreBody Insert
+    Fetched from the getTemplatePreBodyInsert() method
+- Page Body
+    Everything from <body> to </body> (exclusive)
+    Stored in the Page.body member variable
+- Template Post Body Insert
+    Fetched from the getTemplatePostBodyInsert() method
+- Page Close
+    Everything from </body> (inclusive) to the end of the document
+    Stored in the Page.close member variable
+    Probably equal to the fixed string </body>\n</html>\m
+
+In addition, the following details are extracted for use in templating and
+redirect generation:
+- Document title <title>***</title>
+- Page Aliases <meta name="alias" value="PATH-TO-OLD-LOCATION">
+- File path
--- template/main/java/org/directwebremoting/doc/template/Generate.java
+++ template/main/java/org/directwebremoting/doc/template/Generate.java
@@ -2,8 +2,12 @@ package org.directwebremoting.doc.template;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,6 +19,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.directwebremoting.fsguide.FileSystemGuide;
 import org.directwebremoting.fsguide.Visitor;
+import org.directwebremoting.util.CopyUtils;
 
 /**
  * 
@@ -22,50 +27,96 @@ import org.directwebremoting.fsguide.Visitor;
  */
 public class Generate
 {
+    /**
+     * 
+     */
     private static class Page
     {
+        StringBuilder header = new StringBuilder();
+        StringBuilder neck = new StringBuilder();
+        StringBuilder body = new StringBuilder();
+        StringBuilder close = new StringBuilder();
+
         String path;
         String title;
-        String parent;
-        String weight;
         List<String> aliases = new ArrayList<String>();
-        String body;
+
+        Page parent;
+        List<Page> children = new ArrayList<Page>();
 
         @Override
         public String toString()
         {
-            return "Page[path=" + path + ", title=" + title + ", parent=" + parent + ", weight=" + weight + ", aliases=" + aliases + "]";
+            return "Page[path=" + path + ", title='" + title + "', aliases=" + aliases + "]";
+        }
+
+        public String getLink()
+        {
+            return "<a href='http://directwebremoting.org" + path + "'>" + title + "</a>";
+        }
+
+        public String getLink(String text)
+        {
+            return "<a href='http://directwebremoting.org" + path + "'>" + text + "</a>";
         }
     }
 
-    public static void main(String[] args)
+    private static final String ROOT = "ROOT";
+
+    private enum Reading
     {
-        final String root = "/Users/joe/Projects/directwebremoting/docdwr/docs/web";
-        Map<String, Page> pages = readInput(root);
-
-        for (Map.Entry<String, Page> entry : pages.entrySet())
-        {
-            log.info(entry.getKey() + " =");
-            log.info("- " + entry.getValue());
-        }
-
-        
+        Header, Neck, Body, Close
     }
 
     /**
      * 
      */
-    private static Map<String, Page> readInput(final String root)
+    public static void main(String[] args) throws IOException
+    {
+        String root = "/Users/joe/Projects/directwebremoting/docdwr";
+        Map<String, Page> pages = readInput(root + "/docs/web");
+        writeOutput(pages, root + "/target/publish");
+        copyStatic(root + "/docs/web", root + "/target/publish", new String[] {
+            "png", "gif", "jpg", "css", "pdf", "dtd", "xsd", "js"
+        });
+    }
+
+    /**
+     * 
+     */
+    private static void writeOutput(Map<String, Page> pages, String base) throws IOException
+    {
+        for (Page page : pages.values())
+        {
+            File destination = new File(base + page.path);
+            destination.getParentFile().mkdirs();
+
+            PrintWriter out = new PrintWriter(new FileWriter(destination));
+            out.print(page.header);
+            out.print(getTemplateHeaderInsert());
+            out.print(page.neck);
+            out.print(getTemplatePreBodyInsert(page));
+            out.print(page.body);
+            out.print(getTemplatePostBodyInsert(pages.get(ROOT)));
+            out.print(page.close);
+            out.close();
+        }
+    }
+
+    /**
+     * 
+     */
+    private static Map<String, Page> readInput(final String base)
     {
         final Map<String, Page> pages = new HashMap<String, Page>();
 
-        FileSystemGuide guide = new FileSystemGuide(new File(root));
+        FileSystemGuide guide = new FileSystemGuide(new File(base));
         guide.visit(new Visitor()
         {
             public void visitFile(File file)
             {
                 String path = file.getAbsolutePath();
-                path = path.substring(root.length());
+                path = path.substring(base.length());
 
                 if (path.endsWith(".html"))
                 {
@@ -76,88 +127,278 @@ public class Generate
                     try
                     {
                         BufferedReader in = new BufferedReader(new FileReader(file));
-                        StringBuilder body = new StringBuilder();
-                        boolean inBody = false;
+                        Reading reading = Reading.Header;
 
                         while (true)
                         {
                             String line = in.readLine();
                             if (line == null)
                             {
-                                log.warn("Expected </body>");
                                 break;
                             }
-
-                            if (inBody)
+                            
+                            switch (reading)
                             {
+                            case Header:
+                                if (line.matches(" *</head> *"))
+                                {
+                                    reading = Reading.Neck;
+                                    page.neck.append(line);
+                                    page.neck.append("\n");
+                                }
+                                else
+                                {
+                                    page.header.append(line);
+                                    page.header.append("\n");
+
+                                    Matcher titleMatcher = title.matcher(line);
+                                    if (titleMatcher.find())
+                                    {
+                                        page.title = titleMatcher.group(1);
+                                    }
+
+                                    Matcher metaMatcher = meta.matcher(line);
+                                    if (metaMatcher.find())
+                                    {
+                                        String name = metaMatcher.group(1);
+                                        String value = metaMatcher.group(2);
+                                        if ("alias".equals(name))
+                                        {
+                                            page.aliases.add(value);
+                                        }
+                                    }
+                                }
+                                break;
+
+                            case Neck:
+                                page.neck.append(line);
+                                page.neck.append("\n");
+                                if (line.matches(" *<body.*"))
+                                {
+                                    reading = Reading.Body;
+                                }
+                                break;
+
+                            case Body:
                                 if (line.matches(" *</body> *"))
                                 {
-                                    break;
+                                    reading = Reading.Close;
+                                    page.close.append(line);
+                                    page.close.append("\n");
                                 }
-                                body.append(line);
-                                body.append("\n");
-                            }
-                            else
-                            {
-                                if (line.matches(" *<body> *"))
+                                else
                                 {
-                                    inBody = true;
-                                    continue;
+                                    page.body.append(line);
+                                    page.body.append("\n");
                                 }
+                                break;
 
-                                Matcher titleMatcher = title.matcher(line);
-                                if (titleMatcher.find())
-                                {
-                                    page.title = titleMatcher.group(1);
-                                }
-
-                                Matcher metaMatcher = meta.matcher(line);
-                                if (metaMatcher.find())
-                                {
-                                    String name = metaMatcher.group(1);
-                                    String value = metaMatcher.group(2);
-
-                                    if ("nid".equals(name))
-                                    {
-                                        // page.nid = value;
-                                    }
-                                    else if ("parent".equals(name))
-                                    {
-                                        page.parent = value;
-                                    }
-                                    else if ("weight".equals(name))
-                                    {
-                                        page.weight = value;
-                                    }
-                                    else if ("filename".equals(name))
-                                    {
-                                        // page.filename = value;
-                                    }
-                                    else if ("alias".equals(name))
-                                    {
-                                        page.aliases.add(value);
-                                    }
-                                }
+                            case Close:
+                                page.close.append(line);
+                                page.close.append("\n");
+                                break;
                             }
                         }
-
-                        page.body = body.toString();
                     }
                     catch (IOException ex)
                     {
                         log.warn("Parse failed", ex);
                     }
-
-                    log.info("Found " + path);
                 }
             }
 
             public boolean visitDirectory(File directory)
             {
+                if (directory.getName().startsWith("_vti"))
+                {
+                    return false;
+                }
+
                 return true;
             }
         });
+
+        Page root = null;
+        for (Page page : pages.values())
+        {
+            if (page.path.endsWith("index.html"))
+            {
+                String key = new File(page.path).getParentFile().getParent() + "/index.html";
+                page.parent = pages.get(key);
+            }
+            else
+            {
+                String key = new File(page.path).getParent() + "/index.html";
+                page.parent = pages.get(key);
+            }
+
+            if (page.parent == null)
+            {
+                if (root == null)
+                {
+                    root = page;
+                }
+                else
+                {
+                    log.error("More than 1 candidates for tree root:");
+                    log.error("- " + root);
+                    log.error("- " + page);
+                }
+            }
+            else
+            {
+                page.parent.children.add(page);
+            }
+        }
+
+        pages.put(ROOT, root);
         return pages;
+    }
+
+    /**
+     * 
+     */
+    private static void copyStatic(final String source, final String dest, final String[] extensions)
+    {
+        FileSystemGuide guide = new FileSystemGuide(new File(source));
+        guide.visit(new Visitor()
+        {
+            public void visitFile(File file)
+            {
+                String path = file.getAbsolutePath();
+
+                String extra = path.substring(source.length());
+                File toFile = new File(dest + extra);
+
+                boolean matching = false;
+                for (String extension : extensions)
+                {
+                    if (path.endsWith("." + extension))
+                    {
+                        matching = true;
+                    }
+                }
+
+                if (matching)
+                {
+                    try
+                    {
+                        toFile.getParentFile().mkdirs();
+                        CopyUtils.copy(new FileInputStream(file), new FileOutputStream(toFile));
+                    }
+                    catch (IOException ex)
+                    {
+                        log.warn("Failed to copy: " + file.getAbsolutePath(), ex);
+                    }
+                }
+            }
+
+            public boolean visitDirectory(File directory)
+            {
+                if (directory.getName().startsWith("_vti"))
+                {
+                    return false;
+                }
+
+                if (directory.getName().startsWith(".svn"))
+                {
+                    return false;
+                }
+
+                return true;
+            }
+        });
+    }
+
+    /**
+     * 
+     */
+    private static String getTemplateHeaderInsert()
+    {
+        //return "  <script type='text/javascript' src='http://directwebremoting.org/dwr/menu.js'> </script>\n";
+        return "  <script type='text/javascript' src='file:///Users/joe/Projects/directwebremoting/docdwr/docs/web/dwr/menu.js'> </script>\n";
+    }
+
+    private static final String STANDARD_HEADER = 
+        "<div id='header'><a href='http://directwebremoting.org/dwr/'>Direct Web Remoting</a></div>\n" +
+        "<div id=pagelinks>\n" +
+          "<div class='navlinks'>[ \n" +
+             "<a href='/dwr' title='Easy Ajax for Java'>DWR</a> | \n" +
+             "<a href='/blog/joe' title='Joe&#039;s Blog about DWR and Ajax'>Blog</a>\n" +
+           " ]\n" +
+            "<form method='GET' action='http://www.google.com/search' id='search-form'>\n" +
+              "<span id='search'>\n" +
+                "<input class='form-text' type='text' id='search-box' name='as_q' size='20' maxlength='255'>\n" +
+                "<input type='hidden' name='as_sitesearch' value='directwebremoting.org'/>\n" +
+                "<input class='form-submit' type='submit' value='Google'/>\n" +
+              "</span>\n" +
+            "</form>\n" +
+          "</div>\n";
+
+    /**
+     * 
+     */
+    private static String getTemplatePreBodyInsert(Page page)
+    {
+        StringBuilder breadcrumbs = new StringBuilder();
+        Page history = page;
+
+        while (history != null)
+        {
+            if (breadcrumbs.length() != 0)
+            {
+                breadcrumbs.insert(0, " &#x00BB; ");
+            }
+
+            breadcrumbs.insert(0, history.getLink());
+            history = history.parent;
+        }
+
+        return STANDARD_HEADER + "<div id='breadcrumbs'>" + breadcrumbs + "</div></div>\n";
+    }
+
+    /**
+     * 
+     */
+    private static String getTemplatePostBodyInsert(Page root)
+    {
+        StringBuilder menu = new StringBuilder();
+
+        menu.append("<ul class='menu' id='nav'>\n");
+        addMenuOptions(menu, root, "Quick Nav &#x2193;");
+        menu.append("</ul>\n");
+
+        return menu.toString();
+    }
+
+    /**
+     * 
+     */
+    private static void addMenuOptions(StringBuilder menu, Page page, String text)
+    {
+        if (text == null)
+        {
+            text = page.title + " &#x2193;";
+        }
+
+        boolean hasChildren = (page.children.size() > 0);
+        if (hasChildren)
+        {
+            menu.append("<li class='hasChildren'>" + page.getLink(text));
+            menu.append("<ul>\n");
+            for (Page child : page.children)
+            {
+                addMenuOptions(menu, child, null);
+            }
+            menu.append("</ul>\n");            
+            menu.append("</li>\n");
+        }
+        else
+        {
+            menu.append("<li class='noChildren'>");
+            menu.append(page.getLink());
+            menu.append("</li>\n");
+        }
     }
 
     private static final Pattern title = Pattern.compile(" *<title>(.*)</title> *");
