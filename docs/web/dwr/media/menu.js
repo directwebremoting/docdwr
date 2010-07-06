5253e0a7a3ce3ae8d32aad54b1c371852eb23e10
David Marginian <dmarginian@users.noreply.github.com>
2010-07-06 23:29:01 +0000

    Mike's doc changes.

--- docs/web/dwr/media/menu.js
+++ docs/web/dwr/media/menu.js
@@ -1,36 +1,12 @@
 
-/*
-(function() {
-    var menuId = 'nav';
-
-    var init = function() {
-        var link;
-        var subMenus = document.getElementById(menuId).getElementsByTagName('ul');
-        for (var j = 0; j < subMenus.length; j++) {
-            link = subMenus[j].parentNode.getElementsByTagName('a')[0];
-            link.onclick = function(ev) {
-                var parentUl = this.parentNode.getElementsByTagName('ul')[0];
-                if (parentUl.style.display == "block") parentUl.style.display = "none";
-                else parentUl.style.display = "block";
-                return false;
-            };
-            subMenus[j].style.display = "none";
-        }
-    };
-
-    if (window.addEventListener) {
-        window.addEventListener('load', init, false);
-    }
-    else if (window.attachEvent) {
-        window.attachEvent("onload", init);
-    }
-    else {
-        init();
-    }
-})();
-*/
-
 dojo.addOnLoad(function() {
+	// Do JS hover handling (due to IE inabilities)
+	dojo.query("#nav").connect("onmouseenter", function() {
+		dojo.query("#nav").addClass("hovered");
+	});
+	dojo.query("#nav").connect("onmouseleave", function() {
+		dojo.query("#nav").removeClass("hovered");
+	});
     // Hide all the lists in the nav menu and add open/close toggle onclick
     dojo.query("#nav ul").forEach(function(subMenu) {
         dojo.addClass(subMenu, "hidden");
